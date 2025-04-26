66ed4eded40f3b7b85974323d277a485e8a6f94b
udeshika123-cyber <udeshikaprabhashini21@gmail.com>
2025-04-26 10:37:08 +0530

    age and example java file here

--- example.java
+++ example.java
@@ -1,9 +1,9 @@
-class example{
-	public static void main( String[] args){
-		int x =10;
+class example {
+	public static void main(String[] args) {
+		int x = 10;
 		int y = x++;
-		System.out.println("x: "+x);
-		
-		System.out.println("y: "+y);
-		}
+		System.out.println("x: " + x);
+
+		System.out.println("y: " + y);
 	}
+}
